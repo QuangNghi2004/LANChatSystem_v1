790033693e2657e155a3e743094f5dc1623567b9
QuangNghi2004 <Quangnghi099@gmail.com>
2026-04-06 17:01:59 +0700

    Add isTruongPhong() and canManageGroup() methods to ClientConnection.java

---
+++ ClientConnection.java
@@ -0,0 +1,9 @@
+    public boolean isTruongPhong() {
+        // TODO: Implement isTruongPhong method logic here
+        return false;
+    }
+
+    public boolean canManageGroup() {
+        // TODO: Implement canManageGroup method logic here
+        return false;
+    }
